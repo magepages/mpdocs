d291228012aa9e82f78d087264f68c260f54f927
Jimmy <hieunm06@wru.vn>
2019-06-18 17:17:13 +0700

    Update custom.js

--- docs/_static/custom.js
+++ docs/_static/custom.js
@@ -2,7 +2,6 @@
 window.$crisp=[];window.CRISP_WEBSITE_ID="a8100c36-4c44-4f41-9964-bf9c2b32de34";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
 
 var currentSelElm = 'li.current';
-
 var waitFor = setInterval(function() {
     if (jQuery(currentSelElm).length) {
         clearInterval(waitFor);
