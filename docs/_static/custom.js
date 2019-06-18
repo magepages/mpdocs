393739aad130edeb95377214bac6cd6533f8bff9
Jimmy <hieunm06@wru.vn>
2019-06-18 16:24:05 +0700

    Update custom.js

--- docs/_static/custom.js
+++ docs/_static/custom.js
@@ -1,6 +1,7 @@
 // Crisp Chat
 window.$crisp=[];window.CRISP_WEBSITE_ID="a8100c36-4c44-4f41-9964-bf9c2b32de34";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
 
+// jimmy test build
  jQuery('li.current').css({
     'position': 'sticky',
     'top': 0,
