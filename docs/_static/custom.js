8d11eb8c322f02e8bf1dba7bc75d5381180e8dbb
Jimmy <hieunm06@wru.vn>
2019-06-18 16:47:05 +0700

    Update custom.js

--- docs/_static/custom.js
+++ docs/_static/custom.js
@@ -1,10 +1,16 @@
 // Crisp Chat
 window.$crisp=[];window.CRISP_WEBSITE_ID="a8100c36-4c44-4f41-9964-bf9c2b32de34";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
 
-// jimmy test build
- jQuery('li.current').css({
-    'position': 'sticky',
-    'top': 0,
-    'z-index': 999
-});
+var currentSelElm = 'li.current';
+
+var waitFor = setInterval(function() {
+    if (jQuery(currentSelElm).length) {
+        clearInterval(waitFor);
+        jQuery(currentSelElm).css({
+            'position': 'sticky',
+            'top': 0,
+            'z-index': 999
+        });
+    }
+}, 100);
 
